de271ca3b2a2eb84d83a6d3be82fc67775ac1c67
LEANDER KAMMERMEIER <Leander.kammermeier@student.htw-berlin.de>
2021-04-22 14:32:06 +0200

    MakeBricks und RoundSum

---
+++ lab2_Question2/src/lab2_Question2/question2.java
@@ -0,0 +1,22 @@
+package lab2_Question2;
+
+public class question2 {
+
+	public static void main() {
+		
+	}
+	
+	
+	//Make a row of bricks that is GOAL inches long. We have a number of small bricks (1 inch each) and big bricks(5 inches each)
+	//Return true if possible to make the goal by choosing from the given bricks
+	public boolean makeBricks(int small, int big, int goal) {
+		return false;
+		
+	}
+	
+	//Return the sum of the int values a, b, and c which are all to be rounded to the next multiple of 10. 
+	//Write a separate helper "public int round10(int num){}" and call it 3 times for a, b and c respectively, to avoid code repetition
+	public int roundSum(int a, int b, int c) {
+		return 0;
+	}
+}
